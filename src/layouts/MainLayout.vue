1d3bbec8e2e3103d6668d7ffc6f780bc27385d0d
super-valter <valter@valtinho.com.br>
2025-01-10 10:28:03 -0300

    init commit

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,10 +12,8 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          Master Learning
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
@@ -62,36 +60,6 @@ const linksList = [
     icon: 'code',
     link: 'https://github.com/quasarframework'
   },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
 ]
 
 const leftDrawerOpen = ref(false)
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,32 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="flex column flex-center">
+    <h5 class="text-center q-px-md">{{ phrases[phraseIndex] }}</h5>
+    <q-btn icon="autorenew" label="Stacked" stack glossy color="purple" @click="randomPosition" />
   </q-page>
 </template>
 
 <script setup>
-//
+import { onMounted, ref } from 'vue';
+
+const phrases = [
+  'Enfrente os problemas e leve a melhor!',
+  'Os presentes de cada dia',
+  'Dê mais atenção ao que você tem de bom na sua vida',
+  'Não se exija demais',
+  'Um passo por dia',
+  'Para chegar ao topo, o que importa é começar!',
+  'Para hoje, use a roupa da sorte',
+  'De nada adianta ter sonhos, se você não se empenhar em correr atrás',
+  'Preste atenção nas oportunidades que estão à sua frente!',
+  'Positividade para começar o dia é colocar a sua fé em prática'
+]
+let phraseIndex = ref(0)
+
+const randomPosition = () => {
+  phraseIndex.value = Math.floor(Math.random() * phrases.length)
+}
+
+onMounted(() => {
+  randomPosition()
+})
 </script>
